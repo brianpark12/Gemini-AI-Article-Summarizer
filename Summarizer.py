f54d223c949246e17b4878fc15224f563bec3d33
Brian Park <brianytspark@gmail.com>
2026-03-04 23:55:12 -0500

    Update API key loading and user input prompts
    Refactor API key handling and improve user prompts.

--- Summarizer.py
+++ Summarizer.py
@@ -1,15 +1,25 @@
-#By brianpark12
+#By Brian Park
 #Summarizer.py is a script that summarizes the content of a docx or pdf file or a website.
 #Google Gemini is utilized to summarize the contents of an article and extract key points.
 #httpx is used to fetch the contents.
 #os is used to access the API key from the environment variable.
 
-from google import genai
-from google.genai import types
 import httpx
 import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
-client = genai.Client(api_key="Insert your Google Gemini API key here")
+load_dotenv()
+
+key = os.getenv("API_KEY")
+client = genai.Client(api_key = key)
+
+if key is None:
+    print("API KEy is either missing or invalid. Please check .env file and try again.")
+    print("Please format it as API_KEY=your_key_here")
+    exit()
+
 url = input("Please enter the URL of a document or an article you want to summarize (PDF or DOCX format): ").strip()
 information = None
 
@@ -27,7 +37,7 @@ else: #assume it as a website.
     body = httpx.get(url) #extracts the content of the website.
     information = f"The content of the website: {body.text}"
 
-level = input("\nWhat is the target audience for this video? (e.g., middle school or college students, workers in the field: ").strip().lower()
+level = input("\nWhat is the target audience for this video? (e.g., middle school college students, or workers in the field): ").strip().lower()
 prompt = f"This article will be extracted for a script of a science video for {level} level. Precisely summarize the content of the document around 3 sentences, and provide a list of around 5 key points from the document."
 
 response = client.models.generate_content( #creates response based on gemini 3 flash model.
